a05d59a1c46301079ea16f61f4a5a92368f0ef11
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-10 23:00:26 +0530

    Update Armstrong Number.py

--- Armstrong Number.py
+++ Armstrong Number.py
@@ -4,19 +4,18 @@ inputnumber=int(input())    #READING THE INPUT
 temp=inputnumber
 temp1=0
 
-'''          THE ALGORITHM OF FINDING AN ARMSTRONG NUMBER 
-  A number is said to be a armstrong number, if the sum of the cube of the digits is equal to that number.
+                        '''          THE ALGORITHM OF FINDING AN ARMSTRONG NUMBER 
+              A number is said to be a armstrong number, if the sum of the cube of the digits is equal to that number.
  
-     arm=0
-     temp=number
-     while number!=0 do
-           arm+=(number%10)**3
-           number//=10
-     if arm==temp then
-         write "ARMSTRONG NUMBER"
-     else
-         write "NOT AN ARMSTRONG NUMBER"
-                                             '''
+                                              arm=0
+                                              temp=number
+                                              while number!=0 do
+                                                    arm+=(number%10)**3
+                                                    number//=10
+                                              if arm==temp then
+                                                    write "ARMSTRONG NUMBER"
+                                              else
+                                                    write "NOT AN ARMSTRONG NUMBER"          '''
 while temp!=0:
    integer=(temp%10)**3
    temp1+=integer
