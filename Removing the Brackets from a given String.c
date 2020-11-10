2c2c7d093665b23e58595cc441c206f2058a708e
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-10 23:24:16 +0530

    Update Removing the Brackets from a given String.c

--- Removing the Brackets from a given String.c
+++ Removing the Brackets from a given String.c
@@ -1,11 +1,11 @@
-/*
+                            /*
                                                  ### LOGIC OF THE PROGRAM ###
                                                       index <- 0
                                                       while a[index]!='\0' do 
                                                             if a[index] not equals open or close brackets(parantheses,square and curly types) then
                                                                 write a[index]
                                                             increment the index by value 1
-                                                                                                              */
+                                                                                                      */
 #include<stdio.h>
 #include<stdlib.h>
  //Start of the script.....
