f75ea8c1ca0683a038c4cdc25f8ad37008a7d835
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-16 23:54:27 +0530

    Create Printing the ASCII value of the Alphabet String.c

---
+++ Printing the ASCII value of the Alphabet String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+     char input[1000];
+     int i;
+     scanf("%s",a);
+     for(i=0;i<strlen(a);i++)
+     {
+          printf("%d ",a[i]);
+     }
+     return 0;
+}
