490751cef83758f632d4b33a7dfb827c2faa0e96
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-20 23:08:43 +0530

    Create Printing Multiplication table of the given number.cpp

---
+++ Printing Multiplication table of the given number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x,i;
+    std::cin>>n>>>x;
+    for(i=1;i<=x;i++)
+    {
+        std::cout<<n<<"*"<<i<<"="<<(n*i)<<endl;
+    }
+    return 0;
+}
