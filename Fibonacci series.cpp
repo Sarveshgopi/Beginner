71da8806510afdbf472df928f8e779f90177a047
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-23 23:12:58 +0530

    Update Fibonacci series.cpp

--- Fibonacci series.cpp
+++ Fibonacci series.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main()
 {
-    int n,i,t1,t2;
+    int n,i,t1=0,t2=1,nextTerm;
     std::cin>>n;
+    for (i=1;i<=n;i++) 
+    {
+        std::cout<<t1<<" ";
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
     return 0;
     }
